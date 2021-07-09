fe413e82a94b57fa6b21924a36ce46e8e419c4d8
anissa guembre <guembreani@gmail.com>
2021-07-09 14:12:16 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+
+  form.addEventListener('submit',(e) => {
+  e.preventDefault()
+  let li = document.createElement('li')
+
+  li.textContent = form.children[1].value
+  document.querySelector('#tasks').appendChild(li)
+  let delBtn = document.createElement('button')
+
+  li.appendChild(delBtn)
+  delBtn.textContent = "X"
+  delBtn.addEventListener('click', ()=>(li.remove()))
+  document.getElementById("create-task-form").reset()
+  })
+
 });
